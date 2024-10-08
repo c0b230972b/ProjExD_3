87cc1ec24c596d80e8fe3d5a14bd2062285fde5b
c0b23097 <c0b230972b@edu.teu.ac.jp>
2024-10-08 17:31:10 +0900

    追加機能2：途中、エラーあり

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -164,6 +164,7 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
     beam = None
+    beams = []
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
     bomb = Bomb((255, 0, 0), 10)
     score = Score()
@@ -175,7 +176,8 @@ def main():
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 # スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)            
+                beam = Beam(bird)
+                beams.append(beam)       
         screen.blit(bg_img, [0, 0])
         
         for bomb in bombs:
@@ -188,22 +190,28 @@ def main():
                 pg.display.update()
                 time.sleep(5)
                 return
-            
 
         for j, bomb in enumerate(bombs):
-            if beam is not None:
-                if beam.rct.colliderect(bomb.rct):  #ビームと爆弾が衝突したら
-                    beam, bombs[j] = None, None  #bombリストのj番目をNoneにする
-                    bird.change_img(6, screen)
-                    score.score += 1
-                    score.update(screen)
-                    pg.display.update()
-        bombs = [bomb for bomb in bombs if bomb is not None]
+            for k, beam in enumerate(beams):
+                if len(beams) != 0:
+                    if beam.rct.colliderect(bomb.rct):  #ビームと爆弾が衝突したら
+                        beams[k], bombs[j] = None, None  #bombsリストのj番目とbeamsリストのk番目をNoneにする
+                        bird.change_img(6, screen)
+                        score.score += 1
+                        score.update(screen)
+                        pg.display.update()
+            bombs = [bomb for bomb in bombs if bomb is not None]
+        beams = [beam for beam in beams if beam is not None]
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
+        for i, beam in enumerate(beams):
+            if check_bound(beam.rct) != (True, True):
+                del beams[j]
+            else:
+                beam.update(screen)
         score.update(screen)
-        if beam is not None:
+        if len(beams) != 0:
             beam.update(screen) 
         for bomb in bombs:
             bomb.update(screen)
